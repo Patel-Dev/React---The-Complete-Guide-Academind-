dc0145ec65bf125d1f6917957ed42c8b4f0b20a9
Dev <dev200310@gmail.com>
2023-04-18 22:27:14 -0400

    Added work for user data project from Section 8

---
+++ user-data-project/src/App.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+import AddUser from './Components/User/AddUser'
+import UsersList from './Components/User/UsersList'
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, {name: uName, age: uAge, id: Math.random().toString()},];  
+    })
+  }
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </div>
+  );
+}
+
+export default App;
